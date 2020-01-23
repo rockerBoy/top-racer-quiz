ac6f294f141524223477b604ca0326259feb5ffa
rockerBoy <antonpedan01@gmail.com>
2020-01-24 01:27:00 +0200

    Installed Symfony framework

---
+++ api/src/Controller/QuizController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class QuizController extends AbstractController
+{
+    public function index()
+    {
+        dd('Hello World');
+    }
+}
